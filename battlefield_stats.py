17b6738a4dbfdaebd4626ee527a06ef93290fdea
115GOTYOUR6 <jay.oliver2016@outlook.com>
2020-03-12 13:49:32 +0800

    Configuratuion of argparse and URL generation.
    - Some changes were made to argparse to better document the script
    - added a command line argument to facillitate url generation
    - added the creation of the url to retrieve the stats from
    - made some formatting changes for readability

--- battlefield_stats.py
+++ battlefield_stats.py
@@ -1,7 +1,7 @@
 # File: battlefield_stats.py 
 # Author: Jay Oliver
 # Date Created: 11/03/2020
-# Last Modified: 11/03/2020
+# Last Modified: 12/03/2020
 # Purpose: This script is a data visualization tool for the stats
 #          provided on the battlfield tracker website
 # Comments:
@@ -9,14 +9,26 @@
 
 import argparse
 
-parser = argparse.ArgumentParser(description="Process some integers.")
+parser = argparse.ArgumentParser(description="This is a script that "
+                                             "provides a visual "
+                                             "comparison of stats "
+                                             "provided on "
+                                             "battlefieldtracker.com")
 
-parser.add_argument('prof_name', 
-                    help="the name of the battlefield account the "
-                         "stats are for.", 
-                    type=str,
-                    nargs='+')
+parser.add_argument("platform",
+                    help = "The platform that the user account is on.",
+                    type = str,
+                    choices=["origin", "xbox", "psn"])
+
+parser.add_argument("prof_name", 
+                    help = "the name of the battlefield account the "
+                           "stats are for.", 
+                    type = str,
+                    nargs = '+')
 
 args = parser.parse_args()
 
-prof_name = ' '.join(args.prof_name)
+prof_name = " ".join(args.prof_name)
+
+url = ("https://battlefieldtracker.com/bfv/profile/{plat}/{prof}"
+       "/weapons".format(plat = args.platform, prof = prof_name))
