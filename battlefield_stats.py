c84cb8f3b635dc4f379e82cc224bbd028b1c35f3
115GOTYOUR6 <jay.oliver2016@outlook.com>
2020-03-13 22:13:22 +0800

    Url retreival and scrubbing functions.
    The battlefield_stats.py script has been updated to retrieve the
    webpage whose url is partly hard coded. Some fundamental error
    checking has been implimented for the task.
    
    A new directory has been added with the intention of it serving as a
    module and it currently contains the initialization for the webpage
    scrubbing function.

--- battlefield_stats.py
+++ battlefield_stats.py
@@ -1,13 +1,15 @@
 # File: battlefield_stats.py 
 # Author: Jay Oliver
 # Date Created: 11/03/2020
-# Last Modified: 12/03/2020
+# Last Modified: 13/03/2020
 # Purpose: This script is a data visualization tool for the stats
 #          provided on the battlfield tracker website
 # Comments:
 #
 
 import argparse
+import requests
+import sys
 
 parser = argparse.ArgumentParser(description="This is a script that "
                                              "provides a visual "
@@ -32,3 +34,27 @@ prof_name = " ".join(args.prof_name)
 
 url = ("https://battlefieldtracker.com/bfv/profile/{plat}/{prof}"
        "/weapons".format(plat = args.platform, prof = prof_name))
+
+# the url is not expected to change so an option to specify it will not
+# be provided
+try:
+    page = requests.get(url)
+except requests.exceptions.HTTPError as errh:
+    print("Http Error: {}".format(errh))
+    sys.exit(1)
+except requests.exceptions.ConnectionError as errc:
+    print("Connection Error: {}".format(errc))
+    sys.exit(1)
+except requests.exceptions.Timeout as errt:
+    print("Timeout: {}".format(errt))    
+    sys.exit(1)
+except requests.exceptions.TooManyRedirects as errr:
+    print("Redirection Error: {}".format(errr))
+    sys.exit(1)
+except requests.exceptions.RequestException as err:
+    print ("Uknown Error: {}".format(err))
+    sys.exit(1)
+
+if (page.status_code != 200):
+    raise Exception ("Unknown Error: page status "
+                     "received was {}".format(page.status_code))
