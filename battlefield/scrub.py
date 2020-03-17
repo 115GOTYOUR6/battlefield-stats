10d85637560063383113a9d1323418dfec1798ca
115GOTYOUR6 <jay.oliver2016@outlook.com>
2020-03-17 15:02:55 +0800

    Completion of wepage scrubbing.
    The scrubbing of the webpage has been successfully implemented.
    Furthermore some minor changes to the structure of the project
    have been made so as to make the scrub.py module scrub the page and
    pass the information to another function / module for formatting.

--- battlefield/scrub.py
+++ battlefield/scrub.py
@@ -1,7 +1,7 @@
 # File: scrub.py 
 # Author: Jay Oliver
 # Date Created: 13/03/2020
-# Last Modified: 16/03/2020
+# Last Modified: 17/03/2020
 # Purpose: This file contains all functions that relate to web page
 #          scrubbing
 # Comments: More than one scrubbing fuction maybe createdso as to
@@ -17,16 +17,24 @@ def weaps(page):
         and returns them in a dictionary.
 
         parameters:
-            - page:
+            - page: This is the reasponse from the requests.get()
+                    method which contains all the html of the site
 
         returns:
-            - ret:
-
+            - ret: This is a list with entries for each weapon stored
+                   sequentially. It is structed as:
+                   [weap_name, weap_class, kills, kpm, time,
+                   shots_fired, shots_hit, shot_accuracy, headshots...
         raises:
 
     """
 
-    soup = BeautifulSoup(page.content, 'html.parser')
+    soup = BeautifulSoup(page.content, "html.parser")
+    # this entry is one that will appear in the list that is parsed
+    # from the soup and it needs to be removed.
+    bad_ent = BeautifulSoup('<span class="sub" data-v-30f8f9f7="" data-v-7f5c8f78="">--</span>').span
 
-    for x in str(soup.find_all("span", ["name", "sub"])).split(', '):
-        print(x)
+    parsed_soup = [str(i) for i in soup.select('span[data-v-30f8f9f7=""]') if i != bad_ent]
+    ret = [re.search(r">.*<", i).group(0)[1:-1] for i in parsed_soup]
+
+    return ret
