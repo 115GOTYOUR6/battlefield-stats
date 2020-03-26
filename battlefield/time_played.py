43e2241549a5efba8e7b7ed096ee091c40a4d017
115GOTYOUR6 <jay.oliver2016@outlook.com>
2020-03-26 13:04:46 +0800

    Bug fix for format check and play time conversion.
    The bug, returning a false positive on xxs xxs (repeat entries) present in the
    format check method has been fixed and now functions as desired. The addition
    of a day field has also been added to the accepted format such that it is
    now xxd xxh xxm xxs.
    
    A method to convert the play time given in the format above into hours
    has been created and functions as desired.

--- battlefield/time_played.py
+++ battlefield/time_played.py
@@ -1,13 +1,11 @@
 # File: time_played.py
 # Author: Jay Oliver
 # Date Created: 24/03/2020
-# Last Modified: 
+# Last Modified: 26/03/2020
 # Pupose: Contains functions for manipulating a string that represents an
 #         amount of time played
-# Comments: The time played format is, xxh xxm xxs where x is an integer
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# This is incomplete, the format funtion returns a false positive for a string
-# like xxh xxs xxs
+# Comments: The time played format is, xxd xxh xxm xxs where x is an integer.
+#           The order of the time parameters is not considered important
 
 def format(time):
     """Checks if the input parameter is a time played.
@@ -20,24 +18,60 @@ def format(time):
     raises:
     """
 
+    ret = -1
+    temp = []
     try:
         temp = time.split(" ")
     except AttributeError:
-        raise ValueError("The input parameter could not be split.")
-
-    for x in temp:
-        # the implementation is casefold here makes the check non case
-        # sensitive
-        print(x[-1].casefold())
-        if(x[-1].casefold() not in ["h", "m", "s"]):
-            ret = False
-            break
-        else:
-            try:
-                int(x[:-1])
-                ret = True
-            except ValueError:
+        ret = False
+    if(len([i[-1] for i in temp]) > 4 and ret == -1):
+        ret = False
+    elif(len(list(set([i[-1] for i in temp]))) != len(temp) and ret == -1):
+        ret = False
+    elif(ret == -1):
+        for x in temp:
+            # the implementation is casefold here makes the check non case
+            # sensitive
+            if(x[-1].casefold() not in ["d", "h", "m", "s"]):
                 ret = False
                 break
+            else:
+                try:
+                    int(x[:-1])
+                    ret = True
+                except ValueError:
+                    ret = False
+                    break
 
     return ret
+
+
+def hours(time):
+    """Convert a time played into a flaoting point number of hours.
+
+    parameters:
+        - time: The time played that will be converted into hours
+    returns:
+        - total: The time played converted
+    raises:
+    """
+
+    if(format(time)):
+        total = 0
+        values = [int(i[:-1]) for i in time.split(" ")]
+        mults = [i[-1] for i in time.split(" ")]
+        for i in range(len(mults)):
+            if(mults[i] == "d"):
+                mults[i] = 24
+            elif(mults[i] == "h"):
+                mults[i] = 1
+            elif(mults[i] == "m"):
+                mults[i] = 1/60.0
+            else:
+                mults[i] = 1/60.0**2
+        for i in range(len(values)):
+            total = total + values[i] * mults[i]
+    else:
+        raise ValueError("The given parameter was not of the time played"
+                         " format")
+    return total
