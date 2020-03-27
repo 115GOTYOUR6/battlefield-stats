42e9c9d7c494c46a77b119e163548f63e56623b5
115GOTYOUR6 <jay.oliver2016@outlook.com>
2020-03-27 18:05:54 +0800

    Scrub method output formatting and unit test.
    The scrub method has been updated to convert the stats present into an
    appropriate data type such as making the kpm values floating point
    numbers etc.

--- battlefield/scrub.py
+++ battlefield/scrub.py
@@ -1,7 +1,7 @@
 # File: scrub.py 
 # Author: Jay Oliver
 # Date Created: 13/03/2020
-# Last Modified: 21/03/2020
+# Last Modified: 27/03/2020
 # Purpose: This file contains all functions that relate to web page
 #          scrubbing
 # Comments: More than one scrubbing fuction maybe created so as to
@@ -10,7 +10,9 @@
 
 from bs4 import BeautifulSoup
 from re import search
+from re import sub
 
+from battlefield import time_played
 
 def weaps(page):
     """Scrub and return the stats present on battlefield tracker webpage.
@@ -30,7 +32,8 @@ def weaps(page):
                    [weap_name, weap_class, kills, kpm, time,
                    shots_fired, shots_hit, shot_accuracy, headshots...
         raises:
-
+            - valueError: This signifies that there is a stat with a value
+                          is not of the type expected.
     """
     # these are entries that appear in the final parse (ret) that need
     # to be removed
@@ -46,5 +49,26 @@ def weaps(page):
            for i
            in parsed_soup
            if search(r">.*<", i).group(0)[1:-1] not in bad_ent]
+
+    # sanity check and format each of the results
+    if(len(ret) % 9 != 0):
+        raise ValueError("The number of entries in the scrubbed data was not a"
+                         " multiple of 9. This may be the result of the"
+                         " webpage changing its format or an unforseen entry"
+                         " in a weapon stat field.")
+    else:
+        for i in range(int(len(ret)/9)):
+            try:
+                ret[i*9 + 2] = int(sub(",", "", ret[i*9 + 2])) # kills
+                ret[i*9 + 3] = float(ret[i*9 + 3])             # kpm
+                ret[i*9 + 4] = time_played.hours(ret[i*9 + 4]) # time played
+                ret[i*9 + 5] = int(sub(",", "", ret[i*9 + 5])) # shots fired
+                ret[i*9 + 6] = int(sub(",", "", ret[i*9 + 6])) # shots hit
+                ret[i*9 + 7] = float(ret[i*9 + 7])             # accuracy
+                ret[i*9 + 8] = int(sub(",", "", ret[i*9 + 8])) # headshots
+            except ValueError:
+                raise ValueError("One of the stats read in is not of the type"
+                                 " expected.")
+    # perhaps add a function call that puts the list into a dict?
 
     return ret
---
+++ tests/scrub_test.py
@@ -0,0 +1,16 @@
+# this allows an import from the parent directory
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir)
+
+from requests import get
+from battlefield import scrub
+
+
+url = "https://battlefieldtracker.com/bfv/profile/origin/115GOTYOUR6/weapons"
+page = get(url)
+weap_stats = scrub.weaps(page)
+
+for i in weap_stats:
+    print(i)
